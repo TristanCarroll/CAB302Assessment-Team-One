7f33b15ba4dc807ea1b0f629f79f4e37f4c4629c
Tristan Carroll <tjcarroll101@gmail.com>
2024-04-12 18:45:13 +1000

    Initial code commit

---
+++ src/main/java/module-info.java
@@ -0,0 +1,8 @@
+module com.example.byebyeboxeyes {
+    requires javafx.controls;
+    requires javafx.fxml;
+
+
+    opens com.example.byebyeboxeyes to javafx.fxml;
+    exports com.example.byebyeboxeyes;
+}
