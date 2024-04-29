64dc1efd8fa162f9c96fe83a41306ad9e96e74b3
Alex Achille <alex@example.com>
2024-04-29 18:49:45 +1000

    Added private fields and listener setup  to Landing Controller

--- src/main/java/com/example/byebyeboxeyes/controller/LandingController.java
+++ src/main/java/com/example/byebyeboxeyes/controller/LandingController.java
@@ -2,11 +2,48 @@ package com.example.byebyeboxeyes.controller;
 
 import com.example.byebyeboxeyes.StateManager;
 import com.example.byebyeboxeyes.events.EventService;
-
+import com.example.byebyeboxeyes.model.User;
+import com.example.byebyeboxeyes.model.UserDAO;
 import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
+import javafx.geometry.Insets;
+import javafx.scene.control.Alert;
+import javafx.scene.control.Button;
+import javafx.scene.control.Hyperlink;
+import javafx.scene.control.TextField;
+import javafx.scene.layout.StackPane;
+
+import java.sql.SQLException;
+import java.util.Objects;
 
 public class LandingController {
+
+    @FXML
+    private Hyperlink hyperlink;
+    @FXML
+    private StackPane mainStackPane;
+    @FXML
+    private TextField userNameTextField;
+    @FXML
+    private TextField passwordTextField;
+    @FXML
+    private Button loginButton;
+    private final UserDAO userDAO;
+    public LandingController() {
+        userDAO = UserDAO.getInstance();
+        setupListener();
+    }
+
+    /**
+     * Creates a listener for the window height property of the StateManager,
+     * and adjusts the bottom padding accordingly.
+     */
+    private void setupListener() {
+        StateManager.windowHeightProperty().addListener((obs, oldVal, newVal) -> {
+            double paddingValue = newVal.doubleValue() * 0.60; // 60% of height
+            mainStackPane.setPadding(new Insets(20, 20, paddingValue, 20));
+        });
+    }
 
     @FXML
     public void onLoginButtonClick(ActionEvent actionEvent) {
