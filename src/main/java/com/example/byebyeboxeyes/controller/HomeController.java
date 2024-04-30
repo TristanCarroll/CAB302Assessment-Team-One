d676e8f83461f574a38bda8a2533b95e681d31d4
ashwipe <ash.knight151203@gmail.com>
2024-04-30 19:53:16 +1000

    Changed password Text Fields to Password Fields for security. Added new navigation option for Settings + image. Fixed navigation focus issues and changed Settings Button to Timers/Home Button. Resolved merge conflicts.

--- src/main/java/com/example/byebyeboxeyes/controller/HomeController.java
+++ src/main/java/com/example/byebyeboxeyes/controller/HomeController.java
@@ -4,12 +4,14 @@ import com.example.byebyeboxeyes.events.EventService;
 import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
 import javafx.scene.control.Button;
+import javafx.scene.image.ImageView;
+import javafx.scene.input.MouseEvent;
 
 public class HomeController {
     @FXML
-    private Button homeNavButton;
+    private ImageView settingsNavButton;
     @FXML
-    private Button settingsNavButton;
+    private Button timersNavButton;
     @FXML
     private Button statisticsNavButton;
     @FXML
@@ -20,17 +22,21 @@ public class HomeController {
         //  This should be fine as long as the nav buttons are only used for navigation.
         //  If we end up extending their functionality we may need to consider what's passed into the function
         //  to better inform the target which button has been clicked.
-        if (event.getSource() == homeNavButton){
+        if (event.getSource() == timersNavButton){
             EventService.getInstance().notifyNavigationEvent("/com/example/byebyeboxeyes/home-view.fxml");
         }
-        else if (event.getSource() == settingsNavButton){
-            EventService.getInstance().notifyNavigationEvent("/com/example/byebyeboxeyes/settings-view.fxml");
-        }
+//        else if (event.getSource() == settingsNavButton){
+//            EventService.getInstance().notifyNavigationEvent("/com/example/byebyeboxeyes/settings-view.fxml");
+//        }
         else if (event.getSource() == statisticsNavButton){
             EventService.getInstance().notifyNavigationEvent("/com/example/byebyeboxeyes/statistics-view.fxml");
         }
         else if (event.getSource() == goalsNavButton){
             EventService.getInstance().notifyNavigationEvent("/com/example/byebyeboxeyes/goals-view.fxml");
         }
+    }
+
+    public void onImageClick(MouseEvent mouseEvent) {
+        EventService.getInstance().notifyNavigationEvent("/com/example/byebyeboxeyes/settings-view.fxml");
     }
 }
