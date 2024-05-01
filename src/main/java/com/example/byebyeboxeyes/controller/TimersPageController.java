ce9dfb6f54f179fbc8bf1e73f40ee10103cdfc8c
Tristan Carroll <tjcarroll101@gmail.com>
2024-05-01 20:39:43 +1000

    Timers now display in little boxes

--- src/main/java/com/example/byebyeboxeyes/controller/TimersPageController.java
+++ src/main/java/com/example/byebyeboxeyes/controller/TimersPageController.java
@@ -6,8 +6,11 @@ import javafx.animation.Timeline;
 import javafx.fxml.FXML;
 import javafx.fxml.Initializable;
 import javafx.scene.control.Button;
+import javafx.scene.control.Label;
 import javafx.scene.control.ListView;
 import javafx.scene.control.TextField;
+import javafx.scene.layout.FlowPane;
+import javafx.scene.layout.StackPane;
 import javafx.util.Duration;
 
 import java.net.URL;
@@ -24,14 +27,14 @@ public class TimersPageController implements Initializable {
     @FXML private TextField minutesField;
     @FXML private TextField secondsField;
     @FXML private Button createTimerButton;
-    @FXML private ListView<Timer> recentTimersList;
+    @FXML private FlowPane recentTimersFlowPane;
 
     private Timeline timeline;
     private final TimerDAO timerDAO = TimerDAO.getInstance();
 
     @Override
     public void initialize(URL url, ResourceBundle resourceBundle) {
-        recentTimersList.setCellFactory(listView -> new TimerListCell());
+        recentTimersFlowPane.getChildren().clear();
         displayTimersFromDatabase();
     }
 
@@ -49,7 +52,7 @@ public class TimersPageController implements Initializable {
             }
 
             Timer timer = new Timer(StateManager.getCurrentUser().getUserID(), hours, minutes, seconds);
-            recentTimersList.getItems().add(timer);
+            recentTimersFlowPane.getChildren().add(createTimerBox(timer));
             startTimer(timer);
 
             timerDAO.saveTimer(timer);
@@ -64,7 +67,7 @@ public class TimersPageController implements Initializable {
             if (timer.isFinished()) {
                 timeline.stop();
             }
-            recentTimersList.refresh();
+            updateTimerLabel(timer);
         }));
         timeline.setCycleCount(Timeline.INDEFINITE);
         timeline.play();
@@ -72,6 +75,30 @@ public class TimersPageController implements Initializable {
 
     private void displayTimersFromDatabase() {
         List<Timer> allTimers = timerDAO.loadTimers(StateManager.getCurrentUser().getUserID());
-        recentTimersList.getItems().setAll(allTimers);
+        for (Timer timer : allTimers) {
+            recentTimersFlowPane.getChildren().add(createTimerBox(timer));
+            startTimer(timer);
+        }
+    }
+    private StackPane createTimerBox(Timer timer) {
+        StackPane timerPane = new StackPane();
+        timerPane.setStyle("-fx-border-color: black; -fx-border-width: 1px;");
+        timerPane.setPrefSize(100, 50);
+
+        Label timerLabel = new Label(timer.toString());
+        timerLabel.setStyle("-fx-font-size: 14; -fx-font-weight: bold;");
+
+        timerPane.getChildren().add(timerLabel);
+
+        return timerPane;
+    }
+    private void updateTimerLabel(Timer timer) {
+        for (int i = 0; i < recentTimersFlowPane.getChildren().size(); i++) {
+            if (recentTimersFlowPane.getChildren().get(i).getUserData() == timer) {
+                Label timerLabel = (Label) ((FlowPane) recentTimersFlowPane.getChildren().get(i)).getChildren().get(0);
+                timerLabel.setText(timer.toString());
+                break;
+            }
+        }
     }
 }
