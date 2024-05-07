83ec0ec31fcde3c34d0f9cf5a68a004f7d9adf49
Tristan Carroll <tjcarroll101@gmail.com>
2024-05-07 19:28:41 +1000

    Added some buttons to the timer box... Need to extract this out into its own class so it can be re-used for the current timer

--- src/main/java/com/example/byebyeboxeyes/controller/TimersPageController.java
+++ src/main/java/com/example/byebyeboxeyes/controller/TimersPageController.java
@@ -1,17 +1,17 @@
 package com.example.byebyeboxeyes.controller;
 
 import com.example.byebyeboxeyes.StateManager;
-import javafx.animation.KeyFrame;
 import javafx.animation.Timeline;
 import javafx.fxml.FXML;
 import javafx.fxml.Initializable;
+import javafx.geometry.Pos;
 import javafx.scene.control.Button;
 import javafx.scene.control.Label;
-import javafx.scene.control.ListView;
 import javafx.scene.control.TextField;
 import javafx.scene.layout.FlowPane;
+import javafx.scene.layout.HBox;
 import javafx.scene.layout.StackPane;
-import javafx.util.Duration;
+import javafx.scene.layout.VBox;
 
 import java.net.URL;
 import java.util.ResourceBundle;
@@ -25,7 +25,6 @@ public class TimersPageController implements Initializable {
     @FXML private TextField hoursField;
     @FXML private TextField minutesField;
     @FXML private TextField secondsField;
-    @FXML private Button createTimerButton;
     @FXML private FlowPane recentTimersFlowPane;
 
     private Timeline timeline;
@@ -40,9 +39,16 @@ public class TimersPageController implements Initializable {
     @FXML
     public void createNewTimer() {
         try {
-            int hours = Integer.parseInt(hoursField.getText());
-            int minutes = Integer.parseInt(minutesField.getText());
-            int seconds = Integer.parseInt(secondsField.getText());
+
+            int hours;
+            hours = hoursField.getText().isEmpty() ?
+                     0 : Integer.parseInt(hoursField.getText());
+            int minutes;
+            minutes = minutesField.getText().isEmpty() ?
+                    0 : Integer.parseInt(minutesField.getText());
+            int seconds;
+            seconds = secondsField.getText().isEmpty() ?
+                    0 : Integer.parseInt(secondsField.getText());
 
             // Basic validation
             if (hours < 0 || minutes < 0 || minutes > 59 || seconds < 0 || seconds > 59) {
@@ -51,53 +57,55 @@ public class TimersPageController implements Initializable {
             }
 
             Timer timer = new Timer(StateManager.getCurrentUser().getUserID(), hours, minutes, seconds);
-            recentTimersFlowPane.getChildren().add(createTimerBox(timer));
-            startTimer(timer);
+
+            recentTimersFlowPane.getChildren().clear();
+            recentTimersFlowPane.getChildren().add(createTimerContainer(timer));
 
             timerDAO.saveTimer(timer);
 
         } catch (NumberFormatException e) {
+            e.printStackTrace();
         }
-    }
-
-    private void startTimer(Timer timer) {
-        timeline = new Timeline(new KeyFrame(Duration.seconds(1), event -> {
-            timer.decrementTime();
-            if (timer.isFinished()) {
-                timeline.stop();
-            }
-            updateTimerLabel(timer);
-        }));
-        timeline.setCycleCount(Timeline.INDEFINITE);
-        timeline.play();
     }
 
     private void displayTimersFromDatabase() {
         List<Timer> allTimers = timerDAO.loadTimers(StateManager.getCurrentUser().getUserID());
+        int index = recentTimersFlowPane.getChildren().isEmpty() ?
+                0 : recentTimersFlowPane.getChildren().size() - 1;
         for (Timer timer : allTimers) {
-            recentTimersFlowPane.getChildren().add(createTimerBox(timer));
-            startTimer(timer);
+            recentTimersFlowPane.getChildren().add(index, createTimerContainer(timer));
         }
+        Button addButton = new Button("Add");
+        addButton.setOnAction(event -> createNewTimer());
+        recentTimersFlowPane.getChildren().add(addButton);
     }
-    private StackPane createTimerBox(Timer timer) {
+
+    private StackPane createTimerContainer(Timer timer) {
         StackPane timerPane = new StackPane();
         timerPane.setStyle("-fx-border-color: black; -fx-border-width: 1px;");
-        timerPane.setPrefSize(100, 50);
+        timerPane.setPrefSize(200, 50);
 
         Label timerLabel = new Label(timer.toString());
         timerLabel.setStyle("-fx-font-size: 14; -fx-font-weight: bold;");
 
-        timerPane.getChildren().add(timerLabel);
+        Button editButton = new Button("Edit");
+        Button playButton = new Button("Play");
+        Button deleteButton = new Button("Delete");
+
+        deleteButton.setOnAction(Event -> {
+            recentTimersFlowPane.
+        });
+
+        VBox vbox = new VBox(editButton, playButton, deleteButton);
+        vbox.setAlignment(Pos.CENTER);
+        vbox.setSpacing(5);
+
+        HBox hbox = new HBox(timerLabel, vbox);
+        hbox.setAlignment(Pos.CENTER);
+        hbox.setSpacing(5);
+
+        timerPane.getChildren().add(hbox);
 
         return timerPane;
-    }
-    private void updateTimerLabel(Timer timer) {
-        for (int i = 0; i < recentTimersFlowPane.getChildren().size(); i++) {
-            if (recentTimersFlowPane.getChildren().get(i).getUserData() == timer) {
-                Label timerLabel = (Label) ((FlowPane) recentTimersFlowPane.getChildren().get(i)).getChildren().get(0);
-                timerLabel.setText(timer.toString());
-                break;
-            }
-        }
     }
 }
