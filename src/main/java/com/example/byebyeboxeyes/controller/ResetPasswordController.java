70bcdac6418c151a1091372eed214156b0a39700
ashwipe <ash.knight151203@gmail.com>
2024-05-12 23:58:14 +1000

    Added FXML components on Reset Password page. Switched random password generator over to Reset Password Controller. TODO: implement email functionality, upon submission set user password to generated one, make current user details available to Settings Contoller, implement change password on Settings page.

--- src/main/java/com/example/byebyeboxeyes/controller/ResetPasswordController.java
+++ src/main/java/com/example/byebyeboxeyes/controller/ResetPasswordController.java
@@ -1,4 +1,34 @@
 package com.example.byebyeboxeyes.controller;
 
+import com.example.byebyeboxeyes.events.EventService;
+import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
+import javafx.scene.control.TextField;
+import org.apache.commons.text.RandomStringGenerator;
+import static org.apache.commons.text.CharacterPredicates.DIGITS;
+import static org.apache.commons.text.CharacterPredicates.LETTERS;
+
 public class ResetPasswordController {
+    @FXML
+    public TextField resetPwdUsername;
+    @FXML
+    public TextField resetPwdEmail;
+
+    public void onSendEmailClick(ActionEvent actionEvent) {
+        // generate new temporary password
+        String newTempPassword = generateTempPassword();
+        System.out.println(newTempPassword);
+    }
+
+    public void onBackButtonClick(ActionEvent actionEvent) {
+        EventService.getInstance().notifyNavigationEvent("/com/example/byebyeboxeyes/landing-view.fxml");
+    }
+
+    public String generateTempPassword() {
+        RandomStringGenerator generator = new RandomStringGenerator.Builder()
+                .withinRange('0', 'z')
+                .filteredBy(LETTERS, DIGITS)
+                .build();
+        return generator.generate(12);
+    }
 }
--- src/main/java/com/example/byebyeboxeyes/controller/SettingsController.java
+++ src/main/java/com/example/byebyeboxeyes/controller/SettingsController.java
@@ -3,10 +3,7 @@ package com.example.byebyeboxeyes.controller;
 import com.example.byebyeboxeyes.events.EventService;
 import javafx.event.ActionEvent;
 import javafx.scene.control.Button;
-import org.apache.commons.text.RandomStringGenerator;
 
-import static org.apache.commons.text.CharacterPredicates.DIGITS;
-import static org.apache.commons.text.CharacterPredicates.LETTERS;
 
 public class SettingsController {
     public Button logoutButton;
@@ -16,18 +13,9 @@ public class SettingsController {
     }
 
     public void onResetButtonClick(ActionEvent actionEvent) {
-        // generate new temporary password
-        String newTempPassword = generateTempPassword();
-        System.out.println(newTempPassword);
         // get current user
         // User currentUser = userDAO.getUser();
     }
 
-    public String generateTempPassword() {
-        RandomStringGenerator generator = new RandomStringGenerator.Builder()
-                .withinRange('0', 'z')
-                .filteredBy(LETTERS, DIGITS)
-                .build();
-        return generator.generate(12);
-    }
+
 }
