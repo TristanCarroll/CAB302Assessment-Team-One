1f1bf3a2cba31b9e584380f4cdfcca176799c501
ashwipe <ash.knight151203@gmail.com>
2024-05-13 21:19:29 +1000

    Successfully implemented Reset Password feature. TODO: Implement error handling using regex for email on Forgot your Password page.

--- src/main/java/com/example/byebyeboxeyes/controller/SettingsController.java
+++ src/main/java/com/example/byebyeboxeyes/controller/SettingsController.java
@@ -4,13 +4,19 @@ import com.example.byebyeboxeyes.StateManager;
 import com.example.byebyeboxeyes.events.EventService;
 import com.example.byebyeboxeyes.model.User;
 import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
+import javafx.scene.control.Alert;
 import javafx.scene.control.Button;
 import com.example.byebyeboxeyes.model.UserDAO;
+import javafx.scene.control.TextField;
+import static com.example.byebyeboxeyes.controller.SignUpController.passwordHash;
 
 
 public class SettingsController {
     public Button logoutButton;
     private final UserDAO userDAO;
+    @FXML
+    public TextField newUserPassword;
 
     public SettingsController() {
         userDAO = UserDAO.getInstance();
@@ -23,7 +29,13 @@ public class SettingsController {
     public void onResetButtonClick(ActionEvent actionEvent) {
         User currentUser = StateManager.getCurrentUser();
         String userEmail = currentUser.getEmail();
-        // userDAO.updateUserPassword();
+        System.out.println(userEmail);
+        userDAO.updateUserPassword(passwordHash(newUserPassword.getText()), userEmail);
+        Alert alert = new Alert(Alert.AlertType.INFORMATION);
+        alert.setTitle("Reset Password");
+        alert.setHeaderText("Your password was changed.");
+        alert.setContentText("Your password has been successfully updated.");
+        alert.showAndWait();
     }
 
 
